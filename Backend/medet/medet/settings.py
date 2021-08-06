d32f189e692f089c50305936d6d50ab3f13a9b20
absahil <08absahil@gmail.com>
2021-08-06 11:33:36 +0530

    added bootstrap

--- Backend/medet/medet/settings.py
+++ Backend/medet/medet/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'medet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'medet1',
-        'USER': 'ayush1',
-        'PASSWORD': 'ayush_medet',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'medet1',
+#         'USER': 'ayush1',
+#         'PASSWORD': 'ayush_medet',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
@@ -146,5 +146,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER=os.environ.get('MEDET_USER')
-EMAIL_HOST_PASSWORD=os.environ.get('MEDET_PASSWORD')
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
